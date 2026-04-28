63b7c47a76a6f7de4dac4b94b4b215599f4c0952
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-04-28 16:15:38 -0400

    feat: export BrandsService from BrandsModule for improved accessibility

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -13,5 +13,6 @@ import { UsersModule } from '../users/users.module';
   ],
   controllers: [BrandsController],
   providers: [BrandsService],
+  exports: [BrandsService],
 })
 export class BrandsModule {}
