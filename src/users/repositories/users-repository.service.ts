f7257c489d0461f0fec7424acb99a572c39615fd
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-04-28 16:12:52 -0400

    fix: unable to inject the brands model. getModelToken was removed

--- src/users/repositories/users-repository.service.ts
+++ src/users/repositories/users-repository.service.ts
@@ -1,14 +1,13 @@
 import { Model } from 'mongoose';
 import { RepositoryService } from '../../common/repository/repository.service';
 import { User, UserDocument } from '../../common/schemas/users.schema';
-import { getModelToken, InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersRepositoryService extends RepositoryService<UserDocument> {
   constructor(
-    @InjectModel(getModelToken(User.name))
-    private readonly userModel: Model<UserDocument>,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {
     super(userModel);
   }
