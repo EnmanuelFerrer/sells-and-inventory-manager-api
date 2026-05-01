71f1ecbd79b01e6fec3bab42a735e284ffb7a7fa
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-05-01 05:13:43 -0400

    feat: enhance PaginationQueryDto with validation for limit and skip properties

--- src/common/dtos/pagination-query.dto.ts
+++ src/common/dtos/pagination-query.dto.ts
@@ -1,14 +1,19 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
+  @IsInt()
+  @Min(1)
+  @Max(25)
   limit?: number;
 
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
+  @IsInt()
+  @Min(0)
   skip?: number;
 }
