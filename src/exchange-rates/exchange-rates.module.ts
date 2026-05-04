724fb94fd4e5d2d68920d3e6c2cd592cc5a54b1d
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2026-05-04 10:04:22 -0400

    feat: implement the saving of exchange rates in db
    also improve the performance on getting
    VES exchange rate by using only one
    puppeteer browser page to obtain the
    amount and taking screenshot for evidence

--- src/exchange-rates/exchange-rates.module.ts
+++ src/exchange-rates/exchange-rates.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ExchangeRatesService } from './exchange-rates.service';
 import { PuppeteerModule } from '../puppeteer/puppeteer.module';
+import { ExchangeRatesRepositoryService } from './repositories/exchange-rates-repository.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ExchangeRate,
+  ExchangeRateSchema,
+} from '../common/schemas/exchange-rate.schema';
 
 @Module({
-  imports: [PuppeteerModule],
-  providers: [ExchangeRatesService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: ExchangeRate.name, schema: ExchangeRateSchema },
+    ]),
+
+    PuppeteerModule,
+  ],
+  providers: [ExchangeRatesService, ExchangeRatesRepositoryService],
   exports: [ExchangeRatesService],
 })
 export class ExchangeRatesModule {}
--- src/exchange-rates/exchange-rates.service.ts
+++ src/exchange-rates/exchange-rates.service.ts
@@ -2,12 +2,16 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PuppeteerService } from '../puppeteer/puppeteer.service';
 import { ConfigService } from '@nestjs/config';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { ExchangeRatesRepositoryService } from './repositories/exchange-rates-repository.service';
+import { CurrenciesEnum } from '../common/enums/currencies.enum';
 
 @Injectable()
 export class ExchangeRatesService implements OnModuleInit {
   private readonly logger = new Logger(ExchangeRatesService.name);
 
   constructor(
+    private readonly exchangeRatesRepository: ExchangeRatesRepositoryService,
+
     private readonly puppeteerService: PuppeteerService,
     private readonly configService: ConfigService,
   ) {}
@@ -26,14 +30,17 @@ export class ExchangeRatesService implements OnModuleInit {
     await this.getBolivarExchangeRate();
   }
 
-  async getBolivarExchangeRate(): Promise<number> {
+  async getBolivarExchangeRate(): Promise<void> {
     this.logger.debug(
       'About to perform scrapping on Banco Central de Venezuela website to get the exchange rate of VES for USD.',
     );
     await this.puppeteerService.initializeBrowser();
+    const url = this.configService.getOrThrow<string>(
+      'CENTRAL_BANK_OF_VENEZUELA_URL',
+    );
     const exchangeRate: number =
       await this.puppeteerService.getDataFrom<number>(
-        this.configService.getOrThrow<string>('CENTRAL_BANK_OF_VENEZUELA_URL'),
+        url,
         () => {
           const HTMLCollection = document
             .querySelector('#dolar')
@@ -43,9 +50,30 @@ export class ExchangeRatesService implements OnModuleInit {
           const exchangeRate = Number(exchangeRateText?.replace(',', '.'));
           return exchangeRate;
         },
+        false,
       );
-    await this.puppeteerService.closeBrowser();
     this.logger.debug('Exchange rate of VES for USD obtained.');
-    return exchangeRate;
+    await this.puppeteerService.screenshot(url, 'screenshots', true);
+    await this.puppeteerService.closeBrowser();
+    await this.saveExchangeRate(CurrenciesEnum.VES, exchangeRate);
+  }
+
+  async saveExchangeRate(
+    currency: CurrenciesEnum,
+    amount: number,
+  ): Promise<void> {
+    this.logger.debug(`Saving exchange rate for currency: ${currency}.`);
+    const exists = await this.exchangeRatesRepository.exists({ amount });
+    if (exists) {
+      this.logger.debug(
+        'Exchange rate saving aborted because it currently exists.',
+      );
+      return;
+    }
+    await this.exchangeRatesRepository.create({
+      currency,
+      amount,
+    });
+    this.logger.debug('Exchange rate saved.');
   }
 }
